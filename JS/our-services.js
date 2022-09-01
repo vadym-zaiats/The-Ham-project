715ab2796c3d1a61fb28aa83812a7749d48bfe88
Vadym Zaiats <vadyk.za@gmail.com>
2022-09-01 16:22:22 +0300

    Project One JS

---
+++ JS/our-services.js
@@ -0,0 +1,26 @@
+"use strict";
+const parentEl = document.querySelector(".section-3-tabs");
+const tabs = document.querySelectorAll(".tabs-title");
+const items = document.querySelectorAll(".tabs-item");
+
+parentEl.addEventListener("click", (e) => {
+  let currentItem = e.target;
+  console.log(currentItem);
+  let itemId = currentItem.getAttribute("data-item");
+  let currentTab = document.querySelector(itemId);
+
+  if (!currentItem.classList.contains("active")) {
+    tabs.forEach(function (item) {
+      item.classList.remove("active");
+    });
+
+    items.forEach(function (item) {
+      item.classList.remove("active");
+    });
+
+    currentItem.classList.add("active");
+    currentTab.classList.add("active");
+  }
+});
+
+document.querySelector(".tabs-title").click();
