d4668534f3d2853259c9d078ab0748b08755ac86
Vadym Zaiats <vadyk.za@gmail.com>
2022-09-02 12:00:25 +0300

    HTML // CSS ready

--- JS/our-services.js
+++ JS/our-services.js
@@ -2,13 +2,14 @@
 const parentEl = document.querySelector(".section-3-tabs");
 const tabs = document.querySelectorAll(".tabs-title");
 const items = document.querySelectorAll(".tabs-item");
-
+let imgThirdSect = document.querySelector(".image-section-3");
 parentEl.addEventListener("click", (e) => {
   let currentItem = e.target;
   console.log(currentItem);
   let itemId = currentItem.getAttribute("data-item");
+  console.log(itemId);
   let currentTab = document.querySelector(itemId);
-
+  console.log(currentTab);
   if (!currentItem.classList.contains("active")) {
     tabs.forEach(function (item) {
       item.classList.remove("active");
