0c74ff40e992bf35295e7fa66716d75674ef869d
Vadym Zaiats <vadyk.za@gmail.com>
2022-09-05 20:12:39 +0530

    HTML // CSS ready + one JS left

--- JS/photo-collection.js
+++ JS/photo-collection.js
@@ -1,20 +1,30 @@
 "use strict";
 let button = document.querySelector(".section-5-button");
+console.log(
+  document
+    .querySelector(".grid-section-5 img")
+    .classList.contains("image-section-5")
+);
 
 button.addEventListener("click", () => {
   document
     .querySelector(".grid-section-5")
     .classList.replace("grid-section-5", "grid-section-5-more");
+  document.querySelectorAll(".image-section-5").forEach((e) => {
+    e.classList.replace("image-section-5", "image-section-5-more");
+  });
+
   button.style.visibility = "hidden";
 });
 
 const parentEl = document.querySelector(".section-5-tabs");
-const items = document.querySelectorAll(".image-section-5");
+const items = document.querySelectorAll(".grid-section-5 img");
 const tabs = document.querySelectorAll(".tabs-section-5");
 
 parentEl.addEventListener("click", (item) => {
   let someW = item.target;
   let currentItem = item.target.dataset.item;
+  console.log(currentItem);
   if (!someW.classList.contains("active")) {
     tabs.forEach(function (item) {
       item.classList.remove("active");
@@ -24,7 +34,7 @@ parentEl.addEventListener("click", (item) => {
 
   switch (currentItem) {
     case "all":
-      getImages("image-section-5");
+      getImages("image-section-5", "image-section-5-more");
       break;
     case "graphic-design":
       getImages(currentItem);
@@ -41,9 +51,12 @@ parentEl.addEventListener("click", (item) => {
   }
 });
 
-function getImages(className) {
+function getImages(className, className2) {
   items.forEach((item) => {
-    if (item.classList.contains(className)) {
+    if (
+      item.classList.contains(className) ||
+      item.classList.contains(className2)
+    ) {
       item.style.display = "block";
     } else {
       item.style.display = "none";
