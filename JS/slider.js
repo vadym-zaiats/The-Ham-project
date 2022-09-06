442e9f668dd91418f1ee8b67b76454259cd74ec0
Vadym Zaiats <vadyk.za@gmail.com>
2022-09-06 17:40:41 +0530

    HTML ready + one JS left + one CSS left

--- JS/slider.js
+++ JS/slider.js
@@ -1,18 +1,6 @@
 "use strict";
 
-let i = 1;
-for (let li of carousel.querySelectorAll(".carousel-photo")) {
-  li.style.position = "relative";
-  li.insertAdjacentHTML(
-    "beforeend",
-    `<span style="position:absolute;left:0;top:0">${i}</span>`
-  );
-  i++;
-}
-
-/* конфигурация */
 let width = 84; // ширина li в котором картинка
-// let count = 1; // количество картинок по скроллу
 
 let list = carousel.querySelector("ul");
 let listElems = carousel.querySelectorAll("li");
@@ -23,7 +11,6 @@ carousel.querySelector(".arrow-back").onclick = function () {
   if (position === 0) {
     position = -420;
   }
-  // сдвиг влево
   position += width;
   list.style.marginLeft = position + "px";
 };
@@ -32,7 +19,6 @@ carousel.querySelector(".arrow-next").onclick = function () {
   if (position === -336) {
     position = 84;
   }
-  // сдвиг вправо
   position -= width;
   list.style.marginLeft = position + "px";
 };
