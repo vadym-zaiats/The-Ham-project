655aa3c61bbf6386ccb037cba5ff33483d3c79a7
Vadym Zaiats <vadyk.za@gmail.com>
2022-09-07 21:34:16 +0530

    Almost ready

--- JS/slider.js
+++ JS/slider.js
@@ -1,36 +1,36 @@
 "use strict";
+//
+const photoCol = document.querySelector(".photo-collection-carousel");
+const photos = document.querySelectorAll(".carousel-photo");
+const items = document.querySelectorAll(".block");
 
-let persons = document.querySelectorAll(".carousel-photo");
-let items = document.querySelectorAll(".block");
+photoCol.addEventListener("click", (e) => {
+  let currentItem = e.target; // img
+  let itemId = currentItem.getAttribute("data-about"); // #text
+  let currentTab = document.querySelector(itemId); // текст персоны
 
-persons.forEach((person) => {
-  person.addEventListener("click", (e) => {
-    let currentItem = e.target; // img
-    let itemId = currentItem.getAttribute("data-about"); // #text
-    let currentTab = document.querySelector(itemId); // текст персоны
+  if (
+    !currentItem.classList.contains("active") &&
+    e.target.closest(".carousel-photo")
+  ) {
+    photos.forEach((item) => {
+      item.classList.remove("active");
+    });
 
-    if (!currentItem.classList.contains("active")) {
-      persons.forEach((item) => {
-        item.classList.remove("active");
-      });
+    items.forEach((text) => {
+      text.classList.remove("active");
+    });
 
-      items.forEach((text) => {
-        text.classList.remove("active");
-      });
+    document.querySelector(".main-photo").src = e.target.getAttribute("src");
+  }
 
-      document.querySelector(".main-photo").src = e.target.getAttribute("src");
-    }
-
-    currentItem.classList.add("active");
-    currentTab.classList.add("active");
-  });
+  currentItem.classList.add("active");
+  currentTab.classList.add("active");
 });
 
 let width = 88; // ширина li в котором картинка
 let list = document.querySelector(".gallery ul");
 let position = 0; // положение ленты прокрутки
-let activePerson = document.querySelector(".gallery .active");
-console.log(activePerson.nextElementSibling);
 
 document.querySelector(".arrow-back").addEventListener("click", () => {
   if (position === 0) {
