4fc73af02ae1c33531f0da1115b2b28d159253e7
Vadym Zaiats <vadyk.za@gmail.com>
2022-09-09 16:56:38 +0530

    Not ready

--- JS/slider_2.js
+++ JS/slider_2.js
@@ -8,52 +8,27 @@ let width = 88; // ширина li в котором картинка
 let list = document.querySelector(".photo-collection-carousel");
 let position = 0; // положение ленты прокрутки
 
-const firstPhoto = document.querySelector('[data-about="#text1"]');
-const lastPhoto = document.querySelector('[data-about="#text8"]');
-
 photoCol.addEventListener("click", (e) => {
   let currentClick = e.target;
   let itemDataAtt = currentClick.getAttribute("data-about");
-  let currentText = document.querySelector(itemDataAtt);
-  let currentPerson = document.querySelector(
-    ".photo-collection-carousel .active"
-  );
-  //
+  let currentTab = document.querySelector(itemDataAtt);
+  //   ----Назад----
   if (currentClick.classList.contains("arrow-back")) {
-
-    if (position === 0 && firstPhoto.classList.contains("active")) {
+    if (position === 0) {
       position = -440;
-      position += width;
-      list.style.marginLeft = position + "px";
-
-      firstPhoto.classList.remove("active");
-      lastPhoto.classList.add("active");
-
-    } 
-    
-    else {
-      currentPerson.parentElement.previousElementSibling
-        .querySelector(".carousel-photo")
-        .classList.add("active");
-
-      currentPerson.classList.remove("active");
     }
+    position += width;
+    list.style.marginLeft = position + "px";
   }
-
+  //   ----Вперед----
   if (currentClick.classList.contains("arrow-next")) {
-    // currentPerson.parentElement.nextElementSibling
-    //   .querySelector(".carousel-photo")
-    //   .classList.add("active");
-
-    // currentPerson.classList.remove("active");
     if (position === -352) {
       position = 88;
     }
     position -= width;
     list.style.marginLeft = position + "px";
   }
-  //
-  //
+  //   ----по картинке----
   if (
     !currentClick.classList.contains("active") &&
     e.target.closest(".carousel-photo")
@@ -69,8 +44,22 @@ photoCol.addEventListener("click", (e) => {
     document.querySelector(".main-photo").src = e.target.getAttribute("src");
   }
 
-  if (currentClick.classList.contains("carousel-photo")) {
-    currentClick.classList.add("active");
-    currentText.classList.add("active");
-  }
+  currentClick.classList.add("active");
+  currentTab.classList.add("active");
 });
+
+// document.querySelector(".arrow-back").addEventListener("click", () => {
+//   if (position === 0) {
+//     position = -440;
+//   }
+//   position += width;
+//   list.style.marginLeft = position + "px";
+// });
+
+// document.querySelector(".arrow-next").addEventListener("click", () => {
+//   if (position === -352) {
+//     position = 88;
+//   }
+//   position -= width;
+//   list.style.marginLeft = position + "px";
+// });
